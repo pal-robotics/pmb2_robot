01eff97ad71ab9a81016e2f1dc742238ec20a471
Victor Lopez <victor.lopez@pal-robotics.com>
2021-02-15 13:41:45 +0100

    Migrate pmb2_bringup to ROS2

---
+++ pmb2_bringup/launch/joystick_teleop.launch.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2021 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+from launch_pal.include_utils import include_launch_py_description
+
+import os
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('pmb2_bringup')
+    joy_teleop_path = os.path.join(pkg_dir, 'config', 'joy_teleop.yaml')
+
+    declare_cmd_vel = DeclareLaunchArgument('cmd_vel', default_value='input_joy/cmd_vel',
+            description='Joystick cmd_vel topic')
+    declare_teleop_config = DeclareLaunchArgument('teleop_config', 
+            default_value=joy_teleop_path,
+            description='Joystick teleop configuration file')
+    joy_teleop_node = Node(
+            package='joy_teleop', node_executable='joy_teleop',
+            parameters=[LaunchConfiguration('teleop_config')],
+            remappings=[('cmd_vel', LaunchConfiguration('cmd_vel'))])
+
+    ld = LaunchDescription([
+        declare_cmd_vel,
+        declare_teleop_config,
+        joy_teleop_node,
+        ])
+
+    return ld
---
+++ pmb2_bringup/launch/pmb2_bringup.launch.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2021 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch_pal.include_utils import include_launch_py_description
+
+def generate_launch_description():
+    # Create the launch description and populate
+    ld = LaunchDescription([
+        # TODO missing equivalent to ROS1 robot_pose_node
+        # TODO missing equivalent to tf_lookup, but it may have been legacy from tf1
+        include_launch_py_description('pmb2_bringup', ['launch', 'twist_mux.launch.py']),
+        include_launch_py_description('pmb2_bringup', ['launch', 'joystick_teleop.launch.py']),
+        ])
+
+    return ld
