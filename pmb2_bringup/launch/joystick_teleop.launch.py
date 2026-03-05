e65199edc7cd3ba2438f748f9dac1e5942c09a2c
thomaspeyrucain <thomas.peyrucain@pal-robotics.com>
2026-03-05 11:21:12 +0100

    Add pal configuration

--- pmb2_bringup/launch/joystick_teleop.launch.py
+++ pmb2_bringup/launch/joystick_teleop.launch.py
@@ -18,7 +18,6 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_pal.arg_utils import LaunchArgumentsBase
 from launch_ros.actions import Node
 
@@ -51,26 +50,36 @@ def declare_actions(
     launch_description: LaunchDescription, launch_args: LaunchArguments
 ):
 
-    pkg_dir = get_package_share_directory('pmb2_bringup')
-    joy_teleop_path = os.path.join(pkg_dir,  'config', 'joy_teleop', 'joy_teleop.yaml')
+    from launch_pal import get_pal_configuration
+    # PAL helper function to fetch the configuration for this package,
+    # which might be installed also from other packages
+    joy_teleop_config = get_pal_configuration(
+        pkg="joy_teleop",
+        node="joy_teleop",
+        ld=launch_description)
 
     joy_teleop_node = Node(
         package='joy_teleop',
         executable='joy_teleop',
-        parameters=[joy_teleop_path],
-        remappings=[('cmd_vel', LaunchConfiguration('cmd_vel'))],
-    )
+        parameters=joy_teleop_config['parameters'],
+        remappings=joy_teleop_config['remappings'],)
 
     launch_description.add_action(joy_teleop_node)
+
+    joy_config = get_pal_configuration(
+        pkg="joystick",
+        node="joystick",
+        ld=launch_description)
 
     joy_node = Node(
         package='pal_joy',
         executable='game_controller_node',
         name='joystick',
-        parameters=[os.path.join(pkg_dir, 'config', 'joy_teleop', 'joy_config.yaml')],
-    )
+        parameters=joy_config['parameters'])
 
     launch_description.add_action(joy_node)
+
+    pkg_dir = get_package_share_directory('pmb2_bringup')
 
     joystick_analyzer = Node(
         package='diagnostic_aggregator',
